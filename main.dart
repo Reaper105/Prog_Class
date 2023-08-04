627d306a9b5fa4e029c421e2e025762d8fd38315
Reaper105 <141021715+Reaper105@users.noreply.github.com>
2023-08-04 08:48:09 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,117 @@
+import 'dart:io';
+
+void main(){
+  // print("Welcome To Nigeria");
+
+  // var age = 37;
+  // var = name = "Jude"
+
+  // print(age);
+  // print(name);
+
+  // Dart Operatos
+  // Arithmethic Operators
+  // print(10 + 2);
+  // print(10 - 2);
+  // print(10 * 2);
+  // print(10 / 2);
+
+  // Equality Operator
+  // print(10 == 2);
+  // print(10 != 2);
+
+  // Compaerison Operators
+  // print(10 > 2);
+  // print(10 < 2);
+  // print(10 <= 2);
+  // print(10 >= 2);
+
+  // Logical Operator
+  // print((10 < 2));
+  // print((10 < 2));
+
+  // Defining the const declaration in Dart
+  // const aConstNum = 0; //int constant
+  // const bConstBool = true; //bool constant
+  // const cConstString = 'aConstantString'; //String constant
+
+  // print(aConstNum);
+  // print(bConstBool);
+  // print(cConstString);
+
+  // print(aConstNum.runtimeType);
+  // print(bConstBool.runtimeType);
+  // print(cConstString.runtimeType);
+
+
+  // Dart Operators
+  // int num = 10 + 22;
+  // num = num -2;
+  // print(num);
+
+  // num = num % 5;
+  // print(num);
+
+  //relational operators
+  // if(num == 0){
+  //   print('zero');
+  // }
+
+  // num 100;
+  // num *=2;
+  // print(num);
+
+  //Unary Operator
+  // ++num;
+  // num++;
+  // num += 1;
+  // num -= 1;
+  // print(num);
+
+  // Logical && AND logical || operator
+  // if(num > 200 && num < 203){
+  //   print('200 t0 202');
+  // }
+  
+  // != operator
+  // if(num !=100){
+  //   print('num is not equal to 100');
+  // }
+
+  // Control statements
+  /*1.if and else
+  2. for loops
+  3. while and do-while loops
+  4. break and continue
+  5. switch and case
+  6. assert
+  */
+
+
+
+
+  var age = 20;
+
+  if(age > 18){
+    print("Your age is over 18");
+    print("You can proceed to content");
+  }else if(age == 18){
+    print("Your age is 18");
+  }else{
+    print("You can't view ths content");
+    print("This content is X-Rated, you are too young to view");
+  }
+
+
+
+
+
+  /*Assessment
+  declare a string data and output the result via string concatenation
+  declare an int data, carry out the following operations on it;
+
+a. increament
+b. decreament
+c. inequality
+*/
+}
